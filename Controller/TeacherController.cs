1d67feac1fe3c3522414eca0093062ef5171bd51
OG <o.gojayev2000@gmail.com>
2025-03-11 03:25:18 +0100

    Add TeacherController with basic CRUD operations and routing

--- Controller/TeacherController.cs
+++ Controller/TeacherController.cs
@@ -1,6 +1,37 @@
-﻿namespace WebApplication1.Controller;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
 
-public class TeacherController
+namespace WebApplication1.Controller;
+
+
+[Route("api/[controller]")]
+[ApiController]
+public class TeacherController : ControllerBase
 {
-    
+    private readonly M3ManagmentContext _context;
+
+    public TeacherController(M3ManagmentContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Teacher>>> GetTeacher()
+    {
+        return await _context.Teacher.ToListAsync();
+    }
+
+    [HttpGet("{Id}")]
+    public async Task<ActionResult<Teacher>> GetTeacher(int Id)
+    {
+        var teacher = await _context.Teacher.FindAsync();
+
+        if (teacher == null)
+        {
+            return NotFound("Unable to find teacher with the given ID");
+        }
+
+        return teacher;
+    }
 }
