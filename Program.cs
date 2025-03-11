00d69233e863964fb10e568d76000a65464ee42c
OG <o.gojayev2000@gmail.com>
2025-03-12 00:36:43 +0100

    Add database connection string validation and configure DbContext with environment variable

--- Program.cs
+++ Program.cs
@@ -2,17 +2,26 @@ using Microsoft.EntityFrameworkCore;
 using WebApplication1.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION");
+
+if (string.IsNullOrEmpty(connectionString))
+{
+    throw new InvalidOperationException("Database connection string is missing.");
+}
+
+
+
+builder.Services.AddDbContext<M3ManagmentContext>(options =>
+    options.UseNpgsql(connectionString));
+
 builder.Services.AddControllers()
-.AddJsonOptions(options => options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.Preserve);
-
-
+    .AddJsonOptions(options =>
+    options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.Preserve);
 
 // builder.Services.AddDbContext<M3ManagmentContext>(options =>
 // {
 //     options.UseNpgsql(builder.Configuration.GetConnectionString("ConnectionDB"));
 // });
-
-var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION");
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
